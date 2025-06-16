7daf8ddb16b580de1efca795c8bb3dc819bb7aa5
Trent Tuttle <42724138+tretuttle@users.noreply.github.com>
2025-06-16 00:32:05 -0400

    Add clean widget route for iframe embedding

---
+++ src/pages/Widget.tsx
@@ -0,0 +1,11 @@
+import ChatWidget from "@/components/ChatWidget";
+
+const Widget = () => {
+  return (
+    <div className="min-h-screen">
+      <ChatWidget />
+    </div>
+  );
+};
+
+export default Widget;
